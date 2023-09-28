f01b1312ec0fa586f3dd6ced5f4094dbd825fe41
antonior <antonio.jose.rdc@gmail.com>
2023-09-28 16:48:22 -0300

    versão final do curso "API com Django 3: Testes, segurança e lapidações"

--- python-django-drf-escola-projeto_inicial-master/escola/views.py
+++ python-django-drf-escola-projeto_inicial-master/escola/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, generics, status
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -16,6 +18,7 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    http_method_names = ['get','post','put','patch']
 
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -31,6 +34,11 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
     http_method_names = ['get','post','put','path']
+
+    # cache de 20 segundos
+    @method_decorator(cache_page(20))
+    def dispatch(self, *args, **kwargs):
+        return super(MatriculaViewSet, self).dispatch(*args, **kwargs)
 
 class ListaMatriculasAluno(generics.ListAPIView):
     """Listando as matrículas de um aluno ou aluna"""
--- python-django-drf-escola-projeto_inicial-master/setup/settings.py
+++ python-django-drf-escola-projeto_inicial-master/setup/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_URL = '/media/'
+
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
     # 'DEFAULT_PERMISSION_CLASSES': [
@@ -141,10 +144,25 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-        # 'user': '1000/day'
+        'user': '1000/day'
     },
 }
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
 ]
+
+
+# Para utilizar o Redis como cache, descomentar as linhas abaixo:
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379/1',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
+
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+# SESSION_CACHE_ALIAS = "default"
