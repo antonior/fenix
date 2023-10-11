a67a50da7f5548f6ee26a3689c208f851212e80e
antonior <antonio.jose.rdc@gmail.com>
2023-10-11 11:08:40 -0300

    versão final do curso 3 de JavaScript

--- javascript-fokus/script.js
+++ javascript-fokus/script.js
@@ -16,7 +16,7 @@ const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
-let tempoDecorridoEmSegundos = 1500
+let tempoDecorridoEmSegundos = 25*60
 let intervaloId = null
 
 musica.loop = true
@@ -30,7 +30,7 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 1500
+    tempoDecorridoEmSegundos = 25*60
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
